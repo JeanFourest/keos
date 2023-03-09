9481c1ab8117c80fa1fdf214cb01b6ab0ff64789
FOUREST <jmfourest@gmail.com>
2023-03-09 11:03:05 +0100

    monsters dunno

--- Assets/Scenes/Enemy_Damage.cs
+++ Assets/Scenes/Enemy_Damage.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class Enemy_Damage : MonoBehaviour
 {
+    public Transform playerTransform;
+    public Animator animator;
 
+    public int attackDistance = 2;
     public int attackPower = 2;
-    //public PlayerHealth playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector2.Distance(transform.position, playerTransform.position) <= attackDistance)
+        {
+            animator.SetBool("Attack", true);
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
     }
-    private void OnCollisionEnter2D(Collision2D collision) {
-        //if(collision == "Player"){
-            //playerHealth.TakeDamage(attackPower);
-        //}
+
+
+/*
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+            playerHealth.TakeDamage(attackPower);
+        }
     }
+    */
 }
